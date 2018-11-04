3dce5c23b76707c940ae45f28d9d74e71dd5684b
Vedansh Patel <vxp170009@utdallas.edu>
2018-11-03 23:22:05 -0500

    Update pros-challenge/src/code.py

--- pros-challenge/src/code.py
+++ pros-challenge/src/code.py
@@ -3,9 +3,16 @@ from selenium import webdriver
 browser = webdriver.Chrome()
 browser.set_window_position(-2000, 0)
 
-browser.get("https://www.google.com/flights#flt=HOU.DFW.2018-11-19*DFW.HOU.2018-11-23;c:USD;e:1;sd:1;t:f")
+depart = "HOU"
+arrive = "DFW"
+depDateYMD = "2018-11-19"
+arrDateYMD = "2018-11-20"
+
+browser.get("https://www.google.com/flights#flt="+depart+"."+arrive+"."+depDateYMD+"*"+arrive+"."+depart+"."+arrDateYMD+";c:USD;e:1;sd:1;t:f")
+
 browser.implicitly_wait(2)
 nav = browser.find_element_by_class_name("gws-flights-results__best-flights")
 
 print(nav.text)
+
 browser.quit()
