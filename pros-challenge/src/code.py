84ee5349389b09afe8698d706ebcf83f73c07459
Josef Schodek <jschodek@outlook.com>
2018-11-04 00:44:42 -0500

    output to csv

--- pros-challenge/src/code.py
+++ pros-challenge/src/code.py
@@ -1,3 +1,4 @@
+import csv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -6,8 +7,8 @@ options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 browser = webdriver.Chrome(".\\chromedriver.exe", options=options)
 
-depart = "HOU"
-arrive = "DFW"
+depart = "PVR"
+arrive = "LCY"
 depDateYMD = "2018-11-19"
 arrDateYMD = "2018-11-20"
 
@@ -16,6 +17,13 @@ browser.get("https://www.google.com/flights#flt="+depart+"."+arrive+"."+depDateY
 browser.implicitly_wait(2)
 nav = browser.find_element_by_class_name("gws-flights-results__best-flights")
 
-print(nav.text)
+lines = nav.text.splitlines()
+with open('flights.csv', 'w', newline='') as csvfile:
+	csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	csvwriter.writerow(['Time', 'Airline', 'Duration', 'Airports', 'Cost'])
+	index = 3
+	while index+7 < len(lines):
+		csvwriter.writerow([lines[index], lines[index+1], lines[index+3], lines[index+4], lines[index+7]])
+		index += 9
 
 browser.quit()
