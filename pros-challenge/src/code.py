22ca6aa3201f66dd58e27418512b04c56c07ada7
Vedansh Patel <vxp170009@utdallas.edu>
2018-11-04 08:13:22 -0600

    updates

--- pros-challenge/src/code.py
+++ pros-challenge/src/code.py
@@ -17,20 +17,32 @@ browser.get("https://www.google.com/flights#flt="+depart+"."+arrive+"."+depDateY
 
 browser.implicitly_wait(2)
 nav = browser.find_element_by_class_name("gws-flights-results__best-flights")
-
+#print(nav.text)
 lines = nav.text.splitlines()
-with open('flights.csv', 'w', newline='') as csvfile:
-	csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	csvwriter.writerow(['Time', 'Airline', 'Duration', 'Airports', 'Cost'])
-	index = 3
-	while index+7 < len(lines):
-		csvwriter.writerow([lines[index], lines[index+1], lines[index+3], lines[index+4], lines[index+7]])
-		index += 9
-# o=open("flights.txt", "w+")
-# o.write("Time\tAirline\tDuration\tAirports\tCost\n")
-# index = 3
-# while index+7 < len(lines):
-# 	o.write(lines[index]+"\t" +lines[index+1]+"\t" +lines[index+3]+"\t"+ lines[index+4]+"\t"+ lines[index+7]+"\n")
-# 	index += 9
-# o.close()
+# with open('flights.csv', 'w', newline='') as csvfile:
+# 	csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+# 	csvwriter.writerow(['Time', 'Airline', 'Duration', 'Airports', 'Cost'])
+# 	index = 3
+# 	while index+7 < len(lines):
+# 		csvwriter.writerow([lines[index], lines[index+1], lines[index+3], lines[index+4], lines[index+7]])
+# 		index += 8
+o=open("flights.txt", "w+")
+o.write("Time\tAirline\tDuration\tAirports\tCost\n")
+index = 3
+
+count = 0
+
+for array in lines:
+	if "Operated" in array:
+		del lines[lines.index(array)]
+
+len(lines)
+
+while index+5 < len(lines):
+	o.write(lines[index]+"---" +lines[index+1]+"---" +lines[index+2]+"---"+ lines[index+3]+"---"+ lines[index+5]+"\n")
+	#print(lines[index]+"\t" +lines[index+1]+"\t" +lines[index+2]+"\t"+ lines[index+3]+"\t"+ lines[index+5]+"\n")
+	index += 7
+
+#print(lines)
+o.close()
 browser.quit()
